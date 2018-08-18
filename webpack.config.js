2185ae7d1f5065163045ae2ebeefac29ad782409
matatsuna <ev50510@meiji.ac.jp>
2018-08-19 03:12:32 +0900

    add class & add input file

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,7 @@ const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 module.exports = {
   entry: {
     'main': ['babel-polyfill', './src/app.js'],
+    'pdf.worker': './node_modules/pdfjs-dist/build/pdf.worker.entry'
   },
   output: {
     path: __dirname + '/build',
